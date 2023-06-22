1591c075039576e40888aff1e4982742815f14ab
Bichchuran10 <bichchuran10@gmail.com>
2023-06-22 15:02:03 +0530

    feat: tracking users,joining & leaving

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const http = require("http");
 const path = require("path");
 const app = express();
 const socket = require("socket.io");
-const { generateMessage, generateLocationMessage } = require("./utils/messages");
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./utils/users");
 const server = http.createServer(app);
 const port = process.env.port || 3000;
 
@@ -19,24 +28,49 @@ app.use(express.static(publicDirectoryPath));
 
 io.on("connection", (socket) => {
   console.log("New websocket connection");
+  // socket.emit("message", generateMessage("Welcome back!")); //socket.emit sends it to a specific client
+  // socket.broadcast.emit("message", generateMessage("a new user has joined")); //sends it to every connected client except for this one
 
-  socket.emit("message", generateMessage("Welcome!"));
-  socket.broadcast.emit("message", generateMessage("a new user has joined"));
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
+    console.log("hahahaa user : ", user);
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+
+    socket.emit("message", generateMessage("Welcome!")); //socket.emit sends it to a specific client
+    socket.broadcast
+      .to(user.room)
+      .emit("message", generateMessage(`${user.username} has joined`)); //sends it to every connected client in that room except for this one
+
+    callback();
+  });
 
   socket.on("sendMessage", (message, callback) => {
-    io.emit("message", generateMessage(message));
+    io.emit("message", generateMessage(message)); //io.emit -> sends it to every connected client
     callback("Delivered");
   });
   //   });
   socket.on("sendLocation", (coords, callback) => {
     io.emit(
       "locationMessage",
-      generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+      generateLocationMessage(
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+      )
     );
     callback();
   });
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("a user has disconnected"));
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.username} has left!`)
+      );
+    }
   });
 });
 // Start the server
